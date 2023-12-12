8ac8e025b8446d81d17a69475b5f058b87cf762e
MANUGULASATYA23 <145895699+MANUGULASATYA23@users.noreply.github.com>
2023-12-12 23:50:52 +0530

    onclick buttons  12-12-2023

---
+++ regi_form.js
@@ -0,0 +1,8 @@
+function regi_form(){
+    alert(document.getElementById("reg1").value);
+    alert(document.getElementById("reg2").value);
+    alert(document.getElementById("reg3").value);
+    alert(document.getElementById("reg4").value);
+    alert(document.getElementById("reg5").value);
+    alert(document.getElementById("reg6").value);
+}
